da4c98a111faddb80b26fe4584cec0daec4645ce
Adoroteo01 <andreitot13@gmail.com>
2025-02-13 10:38:41 -0800

    refactored tests for transform module

--- tests/packages/converter/test_transform.py
+++ tests/packages/converter/test_transform.py
@@ -1,5 +1,4 @@
-import sys
-
+import pytest
 from pandas import read_excel
 
 from src.packages.converter.transform import (
@@ -8,21 +7,42 @@ from src.packages.converter.transform import (
     import_data,
 )
 
-sys.path.append("./")
 
-expected = read_excel("data/View_My_Courses_1.xlsx", skiprows=2)
-expected2 = read_excel("data/View_My_Courses_2.xlsx", skiprows=5, skipfooter=7)
-expected3 = read_excel("data/View_My_Courses_3.xlsx", skiprows=2)
+def get_test_data():
+    """Returns a list of tuples containing the test data
+
+    tuple values are meant for the read_excel function:
+    read_excel(data, skiprows=x, skipfooter=y)
+    where data is tuple[0], x is tuple[1], and y is tuple[2]
+
+    """
+    return [
+        ("data/View_My_Courses_1.xlsx", 2, 0),
+        ("data/View_My_Courses_2.xlsx", 5, 7),
+        ("data/View_My_Courses_3.xlsx", 2, 0),
+    ]
 
 
-def test_import_data_desktop():
-    assert import_data("data/View_My_Courses_2.xlsx").equals(expected2)
-    # !!! add desktop version of View_My_Courses_3.xlsx
+@pytest.fixture(params=get_test_data())
+def test_data(request):
+    return request.param
 
 
-def test_import_data_mobile():
-    assert import_data("data/View_My_Courses_1.xlsx").equals(expected)
-    assert import_data("data/View_My_Courses_3.xlsx").equals(expected3)
+@pytest.fixture
+def expected(test_data):
+    """returns expected value for a schedule in test_data"""
+    data, start, end = test_data
+    return read_excel(data, skiprows=start, skipfooter=end)
+
+
+@pytest.fixture
+def data_path(test_data):
+    """returns the path of a schedule in test_data"""
+    return test_data[0]
+
+
+def test_import_data(data_path, expected):
+    assert import_data(data_path).equals(expected)
 
 
 def test_find_start():
@@ -33,3 +53,6 @@ def test_find_start():
 def test_find_end():
     assert _find_end("data/View_My_Courses_1.xlsx") == 0
     assert _find_end("data/View_My_Courses_2.xlsx") == 7
+
+
+# TODO: !!! add tests for when users download during sem 1 versus sem 2
