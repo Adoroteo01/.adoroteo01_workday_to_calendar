5aae193894eeae7c7061b5914af49e85337bd49d
Adoroteo01 <andreitot13@gmail.com>
2025-02-13 11:29:43 -0800

    added tests for new find start and find end behaviour

--- tests/packages/converter/test_transform.py
+++ tests/packages/converter/test_transform.py
@@ -36,23 +36,57 @@ def expected(test_data):
 
 
 @pytest.fixture
+def start(test_data):
+    """returns the index of the start of the ubc schedule"""
+    return test_data[1]
+
+
+@pytest.fixture
+def end(test_data):
+    """returns the index of the end of the ubc schedule"""
+    return test_data[2]
+
+
+@pytest.fixture
 def data_path(test_data):
     """returns the path of a schedule in test_data"""
     return test_data[0]
 
 
-def test_import_data(data_path, expected):
+@pytest.fixture
+def data_dataframe(test_data):
+    """returns a dataframe from a schedule path"""
+    return read_excel(test_data[0])
+
+
+# Test the case where a file path is passed to import_data
+def test_import_data_path(data_path, expected):
     assert import_data(data_path).equals(expected)
 
 
-def test_find_start():
-    assert _find_start("data/View_My_Courses_1.xlsx") == 2
-    assert _find_start("data/View_My_Courses_2.xlsx") == 5
+# Test the case where a DataFrame is passed to import_data
+def test_import_data_dataframe(data_dataframe, expected):
+    assert import_data(data_dataframe).equals(expected)
 
 
-def test_find_end():
-    assert _find_end("data/View_My_Courses_1.xlsx") == 0
-    assert _find_end("data/View_My_Courses_2.xlsx") == 7
+# Test the case where a file path is passed to _find_start
+def test_find_start_path(data_path, start):
+    assert _find_start(data_path) == start
+
+
+# Test the case where a DataFrame is passed to _find_start
+def test_find_start_dataframe(data_dataframe, start):
+    assert _find_start(data_dataframe) == start
+
+
+# Test the case where a file path is passed to _find_end
+def test_find_end_path(data_path, end):
+    assert _find_end(data_path) == end
+
+
+# Test the case where a DataFrame is passed to _find_end
+def test_find_end_dataframe(data_dataframe, end):
+    assert _find_end(data_dataframe) == end
 
 
 # TODO: !!! add tests for when users download during sem 1 versus sem 2
